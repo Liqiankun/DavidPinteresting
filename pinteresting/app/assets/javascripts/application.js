2ba21081c6790ef46254269215feff9e2668e4e7
Liqiankun <qiankunli@yahoo.com>
2016-10-02 22:10:27 +0800

    Add masonry

--- pinteresting/app/assets/javascripts/application.js
+++ pinteresting/app/assets/javascripts/application.js
@@ -11,7 +11,12 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
+//= require masonry/jquery.masonry
 //= require_tree .
+
+
+$.turbo.use('turbolinks:load', 'turbolinks:request-start')
