77907a2b598e3caa855318c6a5b870fc517bb003
Liqiankun <qiankunli@yahoo.com>
2016-10-05 19:14:09 +0800

    Sort by created at

--- pinteresting/app/controllers/pins_controller.rb
+++ pinteresting/app/controllers/pins_controller.rb
@@ -5,7 +5,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order("created_at DESC")#sort by created_at
   end
 
   # GET /pins/1
